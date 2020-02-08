4bb206e8212fcd5ca3c5898c0a22d33a1e768a19
peter nuyts <niodad@gmail.com>
2020-02-08 17:09:16 +0100

    Add project files.

---
+++ PdfMerge/MainWindow.xaml.cs
@@ -0,0 +1,97 @@
+﻿using iText.Kernel.Pdf;
+using iText.Kernel.Utils;
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Windows;
+
+namespace PdfMerge
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public List<string> FileList { get; set; }
+        public string SaveFolder = @"C:\temp\";
+        public MainWindow()
+        {
+
+            InitializeComponent();
+            FileList = new List<string>();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var openFileDialog = new OpenFileDialog
+                {
+                    Multiselect = true,
+                    Filter = "Pdf files (*.pdf)|*.pdf|All files (*.*)|*.*",
+                    InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)
+                };
+                if (openFileDialog.ShowDialog() == true)
+                {
+
+                    foreach (string filename in openFileDialog.FileNames)
+                        FileList.Add(filename);
+                }
+
+                RefeshListbox();
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+
+
+        }
+
+        private void RefeshListbox()
+        {
+            FileListBox.ItemsSource = null;
+            FileListBox.ItemsSource = FileList;
+        }
+
+       
+
+        private void Button_Merge_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var pathToSave = $@"{SaveFolder}{filename.Text}.pdf";
+
+                PdfFunctions.Merge(FileList, pathToSave);
+
+                ProcessStartInfo psi = new ProcessStartInfo("explorer.exe", "/n /e,/select," + pathToSave);
+                Process.Start(psi);
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+       
+        }
+
+        private void Button_Delete_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                FileList.Remove((string)FileListBox.SelectedItem);
+                RefeshListbox();
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+        }
+          
+    }
+}
---
+++ PdfMerge/PdfFunctions.cs
@@ -0,0 +1,28 @@
+﻿using iText.Kernel.Pdf;
+using iText.Kernel.Utils;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PdfMerge
+{
+    class PdfFunctions
+    {
+        public static void Merge(List<string> files, String ouputPath)
+        {
+
+            var pdf = new PdfDocument(new PdfWriter(ouputPath));
+            var merger = new PdfMerger(pdf);
+
+            foreach (var file in files)
+            {
+                var pdfDocument = new PdfDocument(new PdfReader(file));
+                merger.Merge(pdfDocument, 1, pdfDocument.GetNumberOfPages());
+                pdfDocument.Close();
+            }
+
+            pdf.Close();
+        }
+
+    }
+}
