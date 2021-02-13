28ced1ba72acfe6b8c1c3d507e2f4acea6b38d97
peter nuyts <niodad@gmail.com>
2021-02-13 19:54:18 +0100

    Clean up

--- PdfMerge/MainWindow.xaml.cs
+++ PdfMerge/MainWindow.xaml.cs
@@ -1,10 +1,9 @@
-﻿using iText.Kernel.Pdf;
-using iText.Kernel.Utils;
-using Microsoft.Win32;
+﻿using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
+using System.Linq;
 using System.Windows;
 
 namespace PdfMerge
@@ -16,13 +15,12 @@ namespace PdfMerge
     {
         public List<string> FileList { get; set; }
         public string SaveFolder = @"C:\temp\";
+        private string initialDirectory = @"C:\Users\peter\Pictures\Scans";
         public MainWindow()
         {
-
             InitializeComponent();
             FileList = new List<string>();
         }
-
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             try
@@ -31,24 +29,20 @@ namespace PdfMerge
                 {
                     Multiselect = true,
                     Filter = "Pdf files (*.pdf)|*.pdf|All files (*.*)|*.*",
-                    InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)
+                    InitialDirectory = initialDirectory
                 };
                 if (openFileDialog.ShowDialog() == true)
                 {
-
                     foreach (string filename in openFileDialog.FileNames)
                         FileList.Add(filename);
+                    initialDirectory = Path.GetDirectoryName(openFileDialog.FileName);
                 }
-
                 RefeshListbox();
             }
             catch (Exception ex)
             {
-
                 MessageBox.Show(ex.Message);
             }
-
-
         }
 
         private void RefeshListbox()
@@ -57,41 +51,70 @@ namespace PdfMerge
             FileListBox.ItemsSource = FileList;
         }
 
-       
-
         private void Button_Merge_Click(object sender, RoutedEventArgs e)
         {
             try
             {
                 var pathToSave = $@"{SaveFolder}{filename.Text}.pdf";
-
                 PdfFunctions.Merge(FileList, pathToSave);
-
                 ProcessStartInfo psi = new ProcessStartInfo("explorer.exe", "/n /e,/select," + pathToSave);
                 Process.Start(psi);
             }
             catch (Exception ex)
             {
-
                 MessageBox.Show(ex.Message);
             }
-       
+        }
+        private void ImagePanel_Drop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                FileList.AddRange(from file in (string[])e.Data.GetData(DataFormats.FileDrop) select file);
+                RefeshListbox();
+            }
         }
 
         private void Button_Delete_Click(object sender, RoutedEventArgs e)
         {
             try
             {
-                FileList.Remove((string)FileListBox.SelectedItem);
+                foreach (var file in FileListBox.SelectedItems)
+                {
+                    FileList.Remove((string)file);
+                }
                 RefeshListbox();
-
             }
             catch (Exception ex)
             {
-
                 MessageBox.Show(ex.Message);
             }
         }
-          
+
+        private void Up_click(object sender, RoutedEventArgs e)
+        {
+            var selectedIndex = FileListBox.SelectedIndex;
+
+            if (selectedIndex > 0 && selectedIndex < FileListBox.Items.Count)
+            {
+                var itemToMoveUp = this.FileListBox.Items[selectedIndex];
+                FileList.Remove((string)itemToMoveUp);
+                FileList.Insert(selectedIndex - 1, (string)itemToMoveUp);
+                RefeshListbox();
+                FileListBox.SelectedIndex = selectedIndex - 1;
+            }
+        }
+        private void Down_click(object sender, RoutedEventArgs e)
+        {
+            var selectedIndex = FileListBox.SelectedIndex;
+
+            if (selectedIndex + 1 < FileListBox.Items.Count && selectedIndex >= 0)
+            {
+                var itemToMoveDown = FileListBox.Items[selectedIndex];
+                FileList.Remove((string)itemToMoveDown);
+                FileList.Insert(selectedIndex + 1, (string)itemToMoveDown);
+                RefeshListbox();
+                FileListBox.SelectedIndex = selectedIndex + 1;
+            }
+        }
     }
 }
